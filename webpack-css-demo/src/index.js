25781b9816623432297ced55fb12f0ab3ebff6f6
Jimmy <root@myWIFI.local>
2016-02-25 08:28:48 +0800

    删除无用文件

--- webpack-css-demo/src/index.js
+++
@@ -1,7 +0,0 @@
-import 'stylesheets/base'
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Container from 'javascripts/container'
-
-ReactDOM.render(<Container />, document.querySelector('#main'))
